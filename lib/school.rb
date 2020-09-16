e02a21346ed9f21d75788153f27e18b4a35b6d59
kylefarmer85 <kylefarmer85@gmail.com>
2020-09-15 22:33:24 -0500

    finished lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster=({})
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(num)
+    return @roster[num]
+  end
+
+  def sort
+    @roster.sort_by{|k,v| v}.to_h
+  end
+
+end
